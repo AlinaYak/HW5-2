c94a3282af751494b2752574a9f573306bc87846
Alina1103master <honor10alina@gmail.ru>
2022-06-06 13:57:21 +0300

    changes files

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -5,15 +5,14 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import lombok.Data;
 import lombok.Value;
-
 
 import java.util.Locale;
 
 import static io.restassured.RestAssured.given;
 
 public class DataGenerator {
+
     private static final RequestSpecification requestSpec = new RequestSpecBuilder()
             .setBaseUri("http://localhost")
             .setPort(9999)
@@ -26,23 +25,16 @@ public class DataGenerator {
     private DataGenerator() {
     }
 
-    @Data
-    @Value
-    public static class Registration {
-        String login;
-        String password;
-        String status;
+    private static void sendRequest(RegistrationDto user) {
+        given() // "дано"
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(user) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
     }
 
-    private static void sendRequest(Registration user) {
-        given()
-                .spec(requestSpec)
-                .body(user)
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(200);
-    }
 
     public static String getRandomLogin() {
         String login = faker.name().firstName();
@@ -54,15 +46,29 @@ public class DataGenerator {
         return password;
     }
 
-    public static Registration getActiveUser(){
-        Registration activeUser = new Registration(faker.name().firstName(), faker.internet().password(), "active");
-        sendRequest(activeUser);
-        return activeUser;
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationDto getUser(String status) {
+            RegistrationDto user = new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+            return user;
+        }
+
+        public static RegistrationDto getRegisteredUser(String status) {
+            RegistrationDto registeredUser = getUser(status);
+            sendRequest(registeredUser);
+            return registeredUser;
+        }
     }
 
-    public static Registration getBlockedUser(){
-        Registration blockedUser = new Registration(faker.name().firstName(), faker.internet().password(), "blocked");
-        sendRequest(blockedUser);
-        return blockedUser;
+
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
     }
+
+
 }
---
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -0,0 +1,92 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static ru.netology.data.DataGenerator.Registration.*;
+import static ru.netology.data.DataGenerator.getRandomLogin;
+import static ru.netology.data.DataGenerator.getRandomPassword;
+
+public class AuthTest {
+
+    @BeforeEach
+    void setup() {
+        Configuration.headless = true;
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+
+        var registeredUser = getRegisteredUser("active");
+        $("[name ='login']").val(registeredUser.getLogin());
+        $("[name ='password']").val(registeredUser.getPassword());
+        $(withText("Продолжить")).click();
+
+        $x("//*[contains(text(),'Личный кабинет')]").should(appear, Duration.ofSeconds(5));
+
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+
+        var notRegisteredUser = getUser("not registered");
+        $("[name ='login']").val(notRegisteredUser.getLogin());
+        $("[name ='password']").val(notRegisteredUser.getPassword());
+        $(withText("Продолжить")).click();
+
+        $("[data-test-id=error-notification] .notification__content").shouldHave(text("Неверно указан логин или пароль"));
+
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+
+        var blockedUser = getRegisteredUser("blocked");
+        $("[name ='login']").val(blockedUser.getLogin());
+        $("[name ='password']").val(blockedUser.getPassword());
+        $(withText("Продолжить")).click();
+
+        $("[data-test-id=error-notification] .notification__content").shouldHave(text("Пользователь заблокирован"));
+
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+        $("[name ='login']").val(wrongLogin);
+        $("[name ='password']").val(registeredUser.getPassword());
+        $(withText("Продолжить")).click();
+
+        $("[data-test-id=error-notification] .notification__content").shouldHave(text("Неверно указан логин или пароль"));
+
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = getRandomPassword();
+        $("[name ='login']").val(registeredUser.getLogin());
+        $("[name ='password']").val(wrongPassword);
+        $(withText("Продолжить")).click();
+
+        $("[data-test-id=error-notification] .notification__content").shouldHave(text("Неверно указан логин или пароль"));
+
+    }
+}
+
